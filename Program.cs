02f8615721f3be47c53d343fe5de09d0afd287d4
EarthW0rm <marco.a.faustino@gmail.com>
2018-11-13 23:38:57 -0200

    Efetuando o NEW do projeto dotnet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oportunidade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
